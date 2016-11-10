15de7cb7c5169fc184ab6cbee3de61e4a7bdb008
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-11-09 18:39:27 -0800

    Add temporary data in ui state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,17 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideCompleted: false,
+    }
+  }
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
   handleSubmit(event) {
     event.preventDefault();
     
@@ -20,7 +31,11 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -28,7 +43,17 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
+
+          <label className="hide-completed">
+            <input type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+
           <form className="new-task"
             onSubmit={this.handleSubmit.bind(this)}
           >
@@ -53,5 +78,6 @@ App.PropTypes = {
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true} }).count(),
   };
 }, App)
